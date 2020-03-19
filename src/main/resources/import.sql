fa96e4d18674d531969fe65ff5bd2d41b5e8ea4b
albertosgm5 <albertosgm5@gmail.com>
2020-03-19 16:21:31 +0100

    base de datos

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,27 +1,14 @@
 
-insert into PROFESOR (NOMBRE, APELLIDOS) values ('Marcos', 'Puente');
-insert into PROFESOR (NOMBRE, APELLIDOS) values ('Roger', 'Pressman');
-insert into PROFESOR (NOMBRE, APELLIDOS) values ('Martin', 'Fawler');
-insert into PROFESOR (NOMBRE, APELLIDOS) values ('Steve', 'Wozniak');
+insert into Cliente (NOMBRE, APELLIDOS) values ('Av. dir 5','Marcosin', 'UUHH5');
+insert into Cliente (NOMBRE, APELLIDOS) values ('Av. dir 15','sinpa', 'UUHH5');
+insert into Cliente (NOMBRE, APELLIDOS) values ('Av. dir 25', 'Fawler', 'UUHH5');
+insert into Cliente (NOMBRE, APELLIDOS) values ('Av. dir 1', 'Wozniak', 'UUHH5');
 
-insert into MODULO (NOMBRE_MODULO) values ('Desarrollo Entono Servidor');
-insert into MODULO (NOMBRE_MODULO) values ('Entornos de desarrollo');
-insert into MODULO (NOMBRE_MODULO) values ('Sistemas Operativos');
-insert into MODULO (NOMBRE_MODULO) values ('Montaje');
-insert into MODULO (NOMBRE_MODULO) values ('Programacion');
-
-insert into EMAIL (DIRECCION_EMAIL,ID_PROFESOR) values ('marcos@des.com',1);
-insert into EMAIL (DIRECCION_EMAIL,ID_PROFESOR) values ('marcosfppf@gmail.com',1);
-insert into EMAIL (DIRECCION_EMAIL,ID_PROFESOR) values ('roger@des.com',2);
-insert into EMAIL (DIRECCION_EMAIL,ID_PROFESOR) values ('martin@des.com',3);
-insert into EMAIL (DIRECCION_EMAIL,ID_PROFESOR) values ('steve@des.com',4);
-insert into EMAIL (DIRECCION_EMAIL,ID_PROFESOR) values ('steve@apple.com',4);
-
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (1, 1);
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (1, 5);
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (1, 3);
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (2, 2);
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (3, 5);
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (3, 2);
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (4, 4);
-insert into PROFESOR_MODULO(ID_PROFESOR, ID_MODULO) values (4, 3);
+insert into Producto(NOMBRE_MODULO) values ('Camara', 53, 34);
+insert into Producto(NOMBRE_MODULO) values ('Pantalla', 153, 3);
+insert into Producto(NOMBRE_MODULO) values ('Teclado Gamer', 253, 4);
+insert into Producto(NOMBRE_MODULO) values ('Portatil hp v7', 353, 30);
+insert into Producto(NOMBRE_MODULO) values ('Portatil Acer 45', 453, 31);
+insert into Producto(NOMBRE_MODULO) values ('Xbox one + 6 juegos', 553, 14);
+insert into Producto(NOMBRE_MODULO) values ('Movil hn7', 653, 3);
+insert into Producto(NOMBRE_MODULO) values ('Movil Apple 78', 753, 4);
--- target/classes/schema.sql
+++ target/classes/schema.sql
@@ -1,39 +1,46 @@
-CREATE DATABASE IF NOT EXISTS `Tienda_Online`;
-USE `Tienda_Online`;
+drop database if exists tienda;
+create database tienda;
+use tienda;
 
-DROP TABLE IF EXISTS `Cliente`;
-CREATE TABLE `Cliente` (
+DROP TABLE IF EXISTS `lineadc`;
+DROP TABLE IF EXISTS `ventas`;
+DROP TABLE IF EXISTS `producto`;
+DROP TABLE IF EXISTS `cliente`;
+
+CREATE TABLE `cliente` (
   `idCliente` bigint(20) NOT NULL AUTO_INCREMENT,
-  `direccion` varchar(45) NOT NULL,
-  `nombreUsuario` varchar(45) NOT NULL,
-  `password` varchar(45) NOT NULL,
+  `direccion` varchar(255) NOT NULL,
+  `nombreUsuario` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
   PRIMARY KEY (`idCliente`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `LineasDeCompra`;
-CREATE TABLE `LineasDeCompra` (
-  `idLineaCompra` bigint(20) NOT NULL AUTO_INCREMENT,
-  `idProducto` bigint(20) NOT NULL,
-  `idVenta` bigint(20) NOT NULL,
-  `precioProducto` decimal(5,0) NOT NULL,
-  PRIMARY KEY (`idLineaCompra`),
-  KEY `FK_idProducto_idx` (`idProducto`),
-  KEY `FK_idVenta_idx` (`idVenta`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-DROP TABLE IF EXISTS `Producto`;
-CREATE TABLE `Producto` (
+CREATE TABLE `producto` (
   `idProducto` bigint(20) NOT NULL AUTO_INCREMENT,
-  `nombreProducto` varchar(45) NOT NULL,
-  `Precio` decimal(5,0) NOT NULL,
-  `Stock` bigint(20) NOT NULL,
+  `nombreProducto` varchar(255) NOT NULL,
+  `precio` float NOT NULL,
+  `stock` int(11) NOT NULL,
   PRIMARY KEY (`idProducto`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-DROP TABLE IF EXISTS `Venta`;
-CREATE TABLE `Venta` (
+CREATE TABLE `ventas` (
   `idVenta` bigint(20) NOT NULL AUTO_INCREMENT,
-  `fechaVenta` datetime NOT NULL,
-  `descuento` varchar(45) NOT NULL,
-  PRIMARY KEY (`idVenta`)
+  `fechaVenta` date NOT NULL,
+  `descuento` float NOT NULL,
+  `idCliente` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`idVenta`),
+  KEY `fk_idCliente` (`idCliente`),
+  CONSTRAINT `fk_idCliente` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `lineadc` (
+  `idLineaCompra` bigint(20) NOT NULL AUTO_INCREMENT,
+  `idProducto` bigint(20) NOT NULL,
+  `idVenta` bigint(20) NOT NULL,
+  `precioProducto` float NOT NULL,
+  PRIMARY KEY (`idLineaCompra`),
+  KEY `idProducto` (`idProducto`),
+  KEY `idVenta` (`idVenta`),
+  CONSTRAINT `idProducto` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
+  CONSTRAINT `idVenta` FOREIGN KEY (`idVenta`) REFERENCES `ventas` (`idVenta`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
