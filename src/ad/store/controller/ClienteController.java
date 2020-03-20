550a9ae9e8f919ad1a4fedcbef213fe4994afbe2
albertosgm5 <albertosgm5@gmail.com>
2020-03-20 12:31:00 +0100

    controlador cliente

---
+++ src/ad/store/controller/ClienteController.java
@@ -0,0 +1,43 @@
+package ad.store.controller;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import ad.store.entity.Cliente;
+import ad.store.service.UserService;
+import ad.store.service.UserServiceImpl;
+
+@Controller
+@RequestMapping(value = "/cliente")
+public class ClienteController {
+
+	@Autowired
+	UserService clienteService;
+
+
+
+	@RequestMapping(method = RequestMethod.GET, value = "/perfil/{id}")
+	public ModelAndView perfilProfesor(@PathVariable("id") long idCliente) {
+
+		ModelAndView mav = new ModelAndView();
+
+		Cliente cliente = UserService.obtenerCliente(idCliente);
+
+		mav.addObject("cliente", cliente);
+		mav.setViewName("cliente_perfil");
+		return mav;
+	}
+
+	
+	
+	
+}
--- src/ad/store/service/UserServiceImpl.java
+++ src/ad/store/service/UserServiceImpl.java
@@ -7,28 +7,74 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import ad.store.dao.ProductoDao;
+import ad.store.entity.Cliente;
 import ad.store.entity.Producto;
+
+import ad.store.dao.ProductoDao;
+import ad.store.dao.UserDao;
+
+import ad.store.entity.Producto;
+import ad.store.entity.Cliente;
 
 @Transactional
 @Service
-public class UserServiceImpl implements ProductoService {
+public class UserServiceImpl implements UserService {
 
 	@Autowired
-	ProductoDao productoDao;
+	private UserDao profesorDao;
+
+	
+
+	@Autowired
+	private ProductoDao moduloDao;
+
+
 
 	@Override
-	public List<Producto> listarProductos() {
-		return productoDao.listarProductos();
+	public Cliente altaCliente(String nombreUsuario, String direccion, String password) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+
 
 	@Override
-	public List<Producto> listarProductosPorNombre(String nombreProducto) {
-		return productoDao.listarProductosPorNombre(nombreProducto);
+	public Cliente obtenerCliente(long idCliente) {
+		// TODO Auto-generated method stub
+		return null;
 	}
+
+
 
 	@Override
-	public Producto obtenerProducto(long idModulo) {
-		return productoDao.find(idModulo);
+	public Cliente editarCliente(Cliente cliente) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
+
+
+	@Override
+	public Boolean eliminarCliente(long idCliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public Cliente logIn(String nombreUsuario, String direccion, String password) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+	@Override
+	public Boolean logOut(Cliente cliente) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
 }
