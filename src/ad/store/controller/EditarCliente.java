d059d361086e858ac93c6f79d67dab0abb9ec123
Dani <Dani@DESKTOP-10B4DBU.home>
2020-04-15 17:36:59 +0200

    Agregacion de la etiqueta @Controller en EditarCliente

--- src/ad/store/controller/EditarCliente.java
+++ src/ad/store/controller/EditarCliente.java
@@ -1,6 +1,7 @@
 package ad.store.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,7 +11,7 @@ import org.springframework.web.servlet.ModelAndView;
 import ad.store.entity.Cliente;
 import ad.store.entity.Producto;
 import ad.store.service.UserService;
-
+@Controller
 public class EditarCliente {
 	private Cliente cli;
 	private Cliente cliente;
