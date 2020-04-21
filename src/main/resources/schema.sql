071dabfd61f190a78e1c064d14b2fa752e6afe54
Dani <danielnieto14@gmail.com>
2020-04-21 17:58:17 +0200

    Modificacion del schema para la creacion de las tablas rol y clientesrol

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,6 +6,8 @@ DROP TABLE IF EXISTS `lineadc`;
 DROP TABLE IF EXISTS `ventas`;
 DROP TABLE IF EXISTS `producto`;
 DROP TABLE IF EXISTS `cliente`;
+DROP TABLE IF EXISTS `rol`;
+DROP TABLE IF EXISTS `cliente_rol`;
 
 CREATE TABLE `cliente` (
   `idCliente` bigint(20) NOT NULL AUTO_INCREMENT,
@@ -54,4 +56,19 @@ CREATE TABLE `lineadc` (
   KEY `idVenta` (`idVenta`),
   CONSTRAINT `idProducto` FOREIGN KEY (`idProducto`) REFERENCES `producto` (`idProducto`),
   CONSTRAINT `idVenta` FOREIGN KEY (`idVenta`) REFERENCES `ventas` (`idVenta`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `rol` (
+  `id_rol` bigint NOT NULL AUTO_INCREMENT,
+  `nombre_rol` varchar(255) NOT NULL,
+  PRIMARY KEY (`id_rol`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE `cliente_rol` (
+  `id_cliente` bigint NOT NULL,
+  `id_rol` bigint NOT NULL,
+  PRIMARY KEY (`id_cliente`,`id_rol`),
+  KEY `fk_rolrol_idx` (`id_rol`),
+  CONSTRAINT `fk_clienterol` FOREIGN KEY (`id_cliente`) REFERENCES `cliente` (`idCliente`) ON DELETE CASCADE,
+  CONSTRAINT `fk_rolrol` FOREIGN KEY (`id_rol`) REFERENCES `rol` (`id_rol`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
