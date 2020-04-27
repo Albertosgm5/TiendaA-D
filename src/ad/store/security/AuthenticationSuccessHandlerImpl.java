0827942916836b90a3f0c97bb652f6defeccb8de
Dani <Dani@DESKTOP-5NHDQ12.home>
2020-04-27 18:38:53 +0200

    Identificacion por login funcionando

--- src/ad/store/security/AuthenticationSuccessHandlerImpl.java
+++ src/ad/store/security/AuthenticationSuccessHandlerImpl.java
@@ -56,7 +56,7 @@ public class AuthenticationSuccessHandlerImpl implements AuthenticationSuccessHa
 		String targetUrl;
 		if (isUser) {
 			session.setAttribute("rol", "user");
-			targetUrl = "/index";
+			targetUrl = "/";
 		} else if (isAdmin) {
 			session.setAttribute("rol", "admin");
 			targetUrl = "/index";
