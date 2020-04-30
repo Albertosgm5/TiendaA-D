9d83265961c8e0b153a357bd35bf0983effa70cd
Dani <Dani@DESKTOP-5NHDQ12.home>
2020-04-30 15:32:39 +0200

    Modificaciones de los HTML

--- src/ad/store/security/SecurityConfig.java
+++ src/ad/store/security/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 	protected void configure(HttpSecurity http) throws Exception {
 	    http
 	            .authorizeRequests()
-	                .antMatchers("/","/index","/compras/**", "/signup","/producto/list","/producto/detallesProducto/**","/style/**").permitAll()
+	                .antMatchers("/","/index","/compras/**", "/signup","/producto/list","/producto/detallesProducto/**","/style/**","/images/**").permitAll()
 	                .antMatchers("/admin/**").hasAuthority("admin")
 	                .anyRequest().authenticated()
 	            .and()
