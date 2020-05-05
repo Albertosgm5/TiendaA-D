a7d9ff49d89d6cfdc84e01d1367591bb11d8b7c6
Dani <Dani@DESKTOP-5NHDQ12.home>
2020-05-05 11:00:29 +0200

    Editar producto completamente funcional y en su lugar y borrado de carrito al realizar la compra

--- src/ad/store/controller/ProductoController.java
+++ src/ad/store/controller/ProductoController.java
@@ -26,21 +26,23 @@ public class ProductoController {
 	private String name;
 	@Autowired
 	ProductoService productoService;
-	
+
 	@RequestMapping("/detallesProducto/{idProducto}")
-	public ModelAndView perfilProducto(@PathVariable ("idProducto") long idProducto) {
+	public ModelAndView perfilProducto(@PathVariable("idProducto") long idProducto) {
 
 		ModelAndView mav = new ModelAndView();
 
 		Producto producto = productoService.obtenerProducto(idProducto);
-		
+
 		mav.addObject("producto", producto);
 		mav.setViewName("detallesproducto");
 		return mav;
 	}
-	
-	@RequestMapping(method = RequestMethod.POST,value="/detallesProducto/{idProducto}")
-	public ModelAndView agregarCesta(@PathVariable ("idProducto") long idProducto,@RequestParam("cantidad") int cantidad,HttpServletResponse response,HttpServletRequest request) throws IOException {
+
+	@RequestMapping(method = RequestMethod.POST, value = "/detallesProducto/{idProducto}")
+	public ModelAndView agregarCesta(@PathVariable("idProducto") long idProducto,
+			@RequestParam("cantidad") int cantidad, HttpServletResponse response, HttpServletRequest request)
+			throws IOException {
 
 		boolean existe = false;
 		ModelAndView mav = new ModelAndView();
@@ -49,14 +51,14 @@ public class ProductoController {
 		Producto productoVista = productoService.obtenerProducto(idProducto);
 		productoCesta.setStock(cantidad2);
 		HttpSession sesion = request.getSession();
-		List<Producto>cProductos = (List<Producto>) sesion.getAttribute("lProductoSession");
-		for(int i = 0; i<cProductos.size();i++) {
-			if(cProductos.get(i).getIdProducto() == productoCesta.getIdProducto()) {
+		List<Producto> cProductos = (List<Producto>) sesion.getAttribute("lProductoSession");
+		for (int i = 0; i < cProductos.size(); i++) {
+			if (cProductos.get(i).getIdProducto() == productoCesta.getIdProducto()) {
 				cProductos.get(i).setStock(cProductos.get(i).getStock() + productoCesta.getStock());
 				existe = true;
 			}
 		}
-		if(!existe) {
+		if (!existe) {
 			cProductos.add(productoCesta);
 		}
 		sesion.setAttribute("lProductoSession", cProductos);
@@ -64,24 +66,25 @@ public class ProductoController {
 		mav.setViewName("detallesproducto");
 		return mav;
 	}
-	
+
 	@RequestMapping(method = RequestMethod.POST, value = "/list")
-	public ModelAndView listarProductos(@RequestParam("producto") String nombreProducto){
+	public ModelAndView listarProductos(@RequestParam("producto") String nombreProducto) {
 
 		ModelAndView mav = new ModelAndView();
-		//var xhr = new XMLHttpRequest();
-		/*xhr.open('PUT','myservice/user/{id}');
-		 * xhr.setRequestHeader();
-		 * */
+		// var xhr = new XMLHttpRequest();
+		/*
+		 * xhr.open('PUT','myservice/user/{id}'); xhr.setRequestHeader();
+		 */
 		List<Producto> lProducto = productoService.listarProductosPorNombre(nombreProducto);
 
 		mav.addObject("productos", lProducto);
 		mav.setViewName("listarproductos");
 		return mav;
 	}
+
 	@RequestMapping(method = RequestMethod.POST, value = "/listAjax")
-	public ModelAndView listarProductosAjax(@RequestParam("producto") String nombreProducto){
-        
+	public ModelAndView listarProductosAjax(@RequestParam("producto") String nombreProducto) {
+
 		ModelAndView mav = new ModelAndView();
 
 		List<Producto> lProducto = productoService.listarProductosPorNombre(nombreProducto);
@@ -91,16 +94,16 @@ public class ProductoController {
 		return mav;
 	}
 
-	@RequestMapping(method = RequestMethod.GET,value = "crear_Producto" )
+	@RequestMapping(method = RequestMethod.GET, value = "crear_Producto")
 	public String signUpView() {
 		return "crear_Producto";
 	}
-	@RequestMapping(method = RequestMethod.POST,value = "crear_Producto")
-	public void handleSignUp(HttpServletRequest request,HttpServletResponse response,@RequestParam("nombreProducto") String nombre, 
-									@RequestParam("precio") float precio,
-									@RequestParam("stock") int stock,
-									@RequestParam("categoria") String categoria,
-									@RequestParam("descripcion") String descripcion) throws IOException {
+
+	@RequestMapping(method = RequestMethod.POST, value = "crear_Producto")
+	public void handleSignUp(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam("nombreProducto") String nombre, @RequestParam("precio") float precio,
+			@RequestParam("stock") int stock, @RequestParam("categoria") String categoria,
+			@RequestParam("descripcion") String descripcion) throws IOException {
 		Producto producto = productoService.crearProducto(nombre, precio, stock, categoria, descripcion);
 
 		ModelAndView mav = new ModelAndView();
@@ -111,63 +114,59 @@ public class ProductoController {
 		HttpSession session = request.getSession();
 
 		session.setAttribute("productoSession", nombre);
-		 response.sendRedirect("/A&DStore/");
+		response.sendRedirect("/A&DStore/");
 	}
-	@RequestMapping(method = RequestMethod.GET,value = "editar_Producto" )
-	public String updateView() {
-		return "editar_Producto";
+
+	@RequestMapping("/editar_Producto/{idProducto}")
+	public ModelAndView updateView(@PathVariable("idProducto") long idProducto) {
+		ModelAndView mav = new ModelAndView();
+		Producto producto = productoService.obtenerProducto(idProducto);
+		mav.addObject("producto", producto);
+		mav.setViewName("editar_Producto");
+		return mav;
 	}
-	@RequestMapping(method = RequestMethod.POST,value = "editar_Producto")
-	public ModelAndView handleEdit(	HttpServletRequest request,HttpServletResponse response,@RequestParam("nombreProducto") String nombre, 
-			@RequestParam("precio") float precio,
-			@RequestParam("stock") int stock,
-			@RequestParam("categoria") String categoria,
-			@RequestParam("descripcion") String descripcion) {
-		
-		HttpSession session = request.getSession();
-		
-		name=(String) session.getAttribute("accountSession");
-		Producto pro = productoService.obtenerProductoPorNombre(name);
-		Long id= pro.getIdProducto();
-		pro.setIdProducto(id);
+
+	@RequestMapping(method = RequestMethod.POST, value = "/editar_Producto/{idProducto}")
+	public void handleEdit(HttpServletRequest request, HttpServletResponse response,
+			@PathVariable("idProducto") long idProducto, @RequestParam("nombreProducto") String nombre,
+			@RequestParam("precio") float precio, @RequestParam("stock") int stock,
+			@RequestParam("categoria") String categoria, @RequestParam("descripcion") String descripcion) throws IOException {
+
+		Producto pro = productoService.obtenerProducto(idProducto);
 		pro.setNombreProducto(nombre);
 		pro.setPrecio(precio);
 		pro.setStock(stock);
 		pro.setCategoria(categoria);
 		pro.setDescripcion(descripcion);
-		
-		Producto producto = productoService.editarProducto(pro);
 
-		ModelAndView mav = new ModelAndView();
-		session.setAttribute("productoSession", pro.getNombreProducto());
-		mav.addObject("account", producto);
-		mav.setViewName("profile");
-		return mav;
+		productoService.editarProducto(pro);
+		response.sendRedirect("/A&DStore/producto/detallesProducto/" + idProducto);
+
 	}
-	@RequestMapping(method = RequestMethod.POST,value = "/agregar")
-	public void cambioSesionProducto(HttpServletRequest request,HttpServletResponse response,
-			@RequestParam("nombreProducto")String cliente, @RequestParam("precio") float precio,
+
+	@RequestMapping(method = RequestMethod.POST, value = "/agregar")
+	public void cambioSesionProducto(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam("nombreProducto") String cliente, @RequestParam("precio") float precio,
 			@RequestParam("stock") int stock, @RequestParam("categoria") String categoria,
 			@RequestParam("descripcion") String descripcion) throws IOException {
 		HttpSession sesion = request.getSession();
-		List<Producto>cProductos = (List<Producto>) sesion.getAttribute("lProductoSession");
+		List<Producto> cProductos = (List<Producto>) sesion.getAttribute("lProductoSession");
 		Producto producto = new Producto(cliente, precio, stock, categoria, descripcion);
 		cProductos.add(producto);
 		sesion.setAttribute("lProductoSession", cProductos);
 		response.sendRedirect("/A&DStore/");
 	}
-	
-	@RequestMapping(value = "/BorrarP")
-	public void handleDelete(HttpServletRequest request,HttpServletResponse response) throws IOException {
-		HttpSession sesion = request.getSession();
-		name=(String) sesion.getAttribute("productoSession");
-		Producto pro = productoService.obtenerProductoPorNombre(name);
-		Long id= pro.getIdProducto();
-		productoService.eliminarProducto(id);
-	    
-			response.sendRedirect("/A&DStore/");
 
+	@RequestMapping(value = "/BorrarP")
+	public void handleDelete(HttpServletRequest request, HttpServletResponse response) throws IOException {
+		HttpSession sesion = request.getSession();
+		name = (String) sesion.getAttribute("productoSession");
+		Producto pro = productoService.obtenerProductoPorNombre(name);
+		Long id = pro.getIdProducto();
+		productoService.eliminarProducto(id);
+
+		response.sendRedirect("/A&DStore/");
 
 	}
-	
+
 }
