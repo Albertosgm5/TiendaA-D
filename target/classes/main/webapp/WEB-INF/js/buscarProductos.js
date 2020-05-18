c1b2c81137db1357a6e0b822c21871219018afac
Dani <Dani@DESKTOP-5NHDQ12>
2020-05-18 19:28:46 +0200

    Buscar producto con ajax funcionando

--- target/classes/main/webapp/WEB-INF/js/buscarProductos.js
+++ target/classes/main/webapp/WEB-INF/js/buscarProductos.js
@@ -8,32 +8,43 @@ $(document).ready(function() {
 function buscarProducto() {
 	var textoProducto = document.getElementById('nproducto').value;
 
-
 	var token = $("meta[name='_csrf']").attr("content");
 	var header = $("meta[name='_csrf_header']").attr("content");
-	
+
 	$(document).ajaxSend(function(e, xhr, options) {
 		xhr.setRequestHeader(header, token);
 	});
 
 	$
 			.ajax({
-				url : "http://localhost:8080/A&DStore/producto/lista/" + textoProducto,
+				url : "http://localhost:8080/A&DStore/producto/lista/"
+						+ textoProducto,
 				type : 'POST',
 				contentType : "application/json; charset=utf-8",
 				cache : false,
 
 				success : function(response) {
+					var element= document.getElementById("listaProductos");
+					while (element.firstChild) {
+					  element.removeChild(element.firstChild);
+					}
 					var lista = []
 					lista = response;
-			        for (var i = 0; i < lista.length; i++) {           
-			            	console.log(lista[i].idProducto + "-" + lista[i].nombreProducto);
-			            	document.getElementsByName("prueba").value = lista[i].nombreProducto;
-			            	document.getElementsByName("prueba2").value = lista[i].nombreProducto;
-			            	$('#prueba').html(lista[i].idProducto + "-" + lista[i].nombreProducto);
-			        }
+					for (var i = 0; i < lista.length; i++) {
+								
+						var fila =	"<div class='card' style='width: 12rem; display: inline-block; margin: 20px;'>"
+									+ "<div><img src='/A&DStore/producto/imagen/"+lista[i].idProducto+"' style='WIDTH: 12rem; HEIGHT: 12rem;'>"
+									+ "</div>"
+									+ "<div class='card-body'>"
+									+ "<h5 class='card-title'>"+lista[i].nombreProducto+"</h5>"
+									+ "<p class='card-text'>"+lista[i].descripcion+"</p>"
+									+ "<a class='btn btn-primary' href='/A&DStore/producto/detallesProducto/"+lista[i].idProducto+"'> Detalles </a>"
+									+ "</div>"
+
+						$('#listaProductos').append(fila);
+					}
 				},
- 
+
 				error : function(xhr, status, error) {
 					console.log("error al recibir el producto");
 				}
